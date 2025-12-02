163744bba9b877c82ec47d8c2a8095b420e4c321
Anggi Permata <anggiianggrainii33@gmail.com>
2025-12-02 22:09:13 +0700

    Add login page UI (FixTrack)

---
+++ proses_login.php
@@ -0,0 +1,9 @@
+<?php
+// Hanya untuk testing tampilan (belum konek database)
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+echo "<h2>Data terkirim!</h2>";
+echo "Email: $email <br>";
+echo "Password: $pass <br>";
+?>
